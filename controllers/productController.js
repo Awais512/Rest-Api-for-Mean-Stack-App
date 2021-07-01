83b3c446b8f547f9122ba4b178bf7bec27fc619b
awais512 <raza.awais25@gmail.com>
2021-07-01 16:16:47 +0500

    Delete Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -47,12 +47,41 @@ const getProduct = asyncHandler(async (req, res) => {
 //@route    PUT /api/v1/products/:id
 //@desc     Update a product
 //@access   Private
-const updateProduct = asyncHandler(async (req, res) => {});
+const updateProduct = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(404).json({ msg: 'Category Not Found' });
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
+
+  if (!product)
+    return res.status(404).json({ sucess: false, msg: 'Product not found' });
+  res.status(200).json(product);
+});
 
 //@route    DELETE /api/v1/products/:id
 //@desc     Delete a product
 //@access   Private
-const deleteProduct = asyncHandler(async (req, res) => {});
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findByIdAndRemove(req.params.id);
+  if (!product)
+    return res.status(404).json({ sucess: false, msg: 'Product not found' });
+  res.status(200).json({ success: true, msg: 'Product deleted Successfully' });
+});
 
 module.exports = {
   createProduct,
