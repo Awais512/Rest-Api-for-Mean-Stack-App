671fd22b1aded211fc07633b27a6dedbf1d02f28
awais512 <raza.awais25@gmail.com>
2021-07-02 17:43:42 +0500

    Login with Jwt Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //@route    POST /api/v1/auth/register
 //@desc     Register a User
@@ -26,6 +27,23 @@ const register = asyncHandler(async (req, res) => {
 //@route    POST /api/v1/auth/login
 //@desc       Register a User
 //@access     Public
-const login = asyncHandler(async (req, res) => {});
+const login = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.JWT_SECRET;
+  if (!user) return res.status(400).json({ msg: 'User not found' });
+
+  //Compare Password
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      secret
+    );
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    return res.status(400).send('Invalid Password');
+  }
+});
 
 module.exports = { register, login };
