7214510d541062769e092261e76e9bd26fea9ef4
awais512 <raza.awais25@gmail.com>
2021-07-04 11:43:04 +0500

    Delete Order with Order Items

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -81,4 +81,48 @@ const getSingleOrder = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, count: order });
 });
 
-module.exports = { createOrder, getAllOrders, getSingleOrder };
+//@route      PUT /api/v1/orders/:id
+//@desc       Update Single Order Status
+//@access     Private
+const updateOrderStatus = asyncHandler(async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+
+  if (!order) return res.status(400).send('the order cannot be update!');
+
+  res.status(200).json({ success: true, order });
+});
+
+const deleteOrder = asyncHandler((req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200)
+          .json({ success: true, message: 'the order is deleted!' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'order not found!' });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
+});
+
+module.exports = {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+  updateOrderStatus,
+  deleteOrder,
+};
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,9 +6,15 @@ const {
   createOrder,
   getAllOrders,
   getSingleOrder,
+  updateOrderStatus,
+  deleteOrder,
 } = require('../controllers/orderController');
 
 router.route('/').get(getAllOrders).post(createOrder);
-router.route('/:id').get(getSingleOrder);
+router
+  .route('/:id')
+  .get(getSingleOrder)
+  .put(updateOrderStatus)
+  .delete(deleteOrder);
 
 module.exports = router;
